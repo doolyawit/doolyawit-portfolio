239d9bf31ea96fb633c4b8658fb18d99526a32ec
Doolyawit Yodkaew <dew.doolyawit@gmail.com>
2023-07-03 23:12:03 +0700

    ui: add hover effect for menu dark theme

--- src/components/navigation/MenuItem.tsx
+++ src/components/navigation/MenuItem.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useContext } from 'react';
 import HoverLineSvg from './svg/HoverLineSvg';
+import { ThemeContext } from '@/contexts/ThemeContext';
 
 interface MenuItem {
   label: string;
@@ -13,6 +14,7 @@ interface Props {
 }
 
 const Menu: React.FC<Props> = ({ menuItems, height }) => {
+  const { theme } = useContext(ThemeContext);
   return (
     <div className={`flex h-[${height}px] w-1/3 flex-1  gap-2 p-4`}>
       <div className="flex items-center justify-center space-x-4">
@@ -23,7 +25,7 @@ const Menu: React.FC<Props> = ({ menuItems, height }) => {
           >
             <Link href={item.link}>{item.label}</Link>
             <HoverLineSvg
-              color="ffda58"
+              color={theme ? '14D87A' : 'ffda58'}
               width="70"
               height="16"
               className="invisible mx-auto group-hover:visible"

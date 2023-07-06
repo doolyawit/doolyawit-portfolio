fc0c157ff5bb7d3d49d3caa66551218641d65083
Jedsada Tiwongvorakul <jedsada.t@20scoops.net>
2023-07-06 18:29:04 +0700

    fix: position getintouch section

--- app/[lng]/components/contact/Contact.tsx
+++ app/[lng]/components/contact/Contact.tsx
@@ -7,12 +7,12 @@ import { Glass } from './svg/Glass';
 function Contact() {
   return (
     <div className="col-span-10 col-start-2 row-span-3 row-start-2 flex h-full w-full flex-col">
-      <div className="relative mb-[70px] h-1/3 w-full">
-        <div className="absolute left-[7%] 2xl:left-[18%]">
+      <div className="relative mb-[70px] h-1/3 w-full 2xl:mb-0">
+        <div className="absolute left-[7%] 2xl:left-[18%] 3xl:left-[26%]">
           <GetInTouchGraphics />
         </div>
         <div className="absolute h-full w-full">
-          <div className="flex h-full w-full items-center justify-center">
+          <div className="flex h-full w-full items-end justify-center 2xl:-mt-6 2xl:items-center">
             <ContactNavBar />
           </div>
         </div>

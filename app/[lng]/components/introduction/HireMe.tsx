62e41e2143dba8dbb99e119c41b6a638ab390023
Doolyawit Yodkaew <dew.doolyawit@gmail.com>
2023-07-07 16:29:06 +0700

    style: add padding of short descriptiion container

--- app/[lng]/components/introduction/HireMe.tsx
+++ app/[lng]/components/introduction/HireMe.tsx
@@ -6,7 +6,7 @@ export default async function HireMe({ lng }: BaseProp) {
   const { t } = await useTranslation(lng);
   return (
     <div className="mb-8">
-      <p className="mb-4 px-32 py-2 text-center font-st text-body font-normal text-gray dark:text-beige">
+      <p className="mb-4 px-48 py-2 text-center font-st text-body font-normal text-gray dark:text-beige">
         {t('lorem')}
       </p>
       <HireMeButton label={t('hire_me')} />

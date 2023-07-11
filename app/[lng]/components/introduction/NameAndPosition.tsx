817cfa5889b05bc7d0ebc84c376b239ba7f02a01
Doolyawit Yodkaew <dew.doolyawit@gmail.com>
2023-07-11 19:39:32 +0700

    style: adjust responsive design of introduction section

--- app/[lng]/components/introduction/NameAndPosition.tsx
+++ app/[lng]/components/introduction/NameAndPosition.tsx
@@ -7,12 +7,12 @@ export default async function NameAndPosition({ lng }: BaseProp) {
   const { t } = await useTranslation(lng);
 
   return (
-    <div className="h-full w-full">
+    <div className=" h-1/2 w-full xl:h-fit">
       <Position greetingWord={t('greeting')} position={t('front_end')} department={t('developer')} />
-      <div className="ml-56 mt-4 flex w-full text-orange dark:text-green lg:ml-80 xl:-mt-6 xl:ml-[360px]">
+      <div className="ml-56 mt-2 flex w-full text-orange dark:text-green lg:ml-80 xl:-mt-6 xl:ml-[360px]">
         <StarSvg className="h-10 w-10 lg:h-12 lg:w-12 xl:h-14 xl:w-14" />
       </div>
-      <div className="flex w-full justify-center pt-4 text-center font-jk  text-5xl font-extrabold leading-normal text-gray selection:bg-green dark:text-beige dark:selection:bg-orange sm:px-20 sm:text-6xl sm:leading-relaxed xl:px-0 xl:pt-16 xl:text-head1">
+      <div className="flex w-full justify-center pt-4 text-center font-jk  text-5xl font-extrabold leading-normal text-gray selection:bg-green dark:text-beige dark:selection:bg-orange sm:px-20 sm:text-6xl sm:leading-relaxed lg:text-7xl xl:px-0 xl:pt-16 xl:text-head1">
         {t('doolyawit_yodkaew')}
       </div>
     </div>
--- app/[lng]/components/introduction/Position.tsx
+++ app/[lng]/components/introduction/Position.tsx
@@ -9,10 +9,10 @@ export default function Position({
 }) {
   return (
     <>
-      <div className="text-right font-st font-light text-gray dark:text-beige sm:text-xl lg:text-2xl xl:text-caption">
+      <div className="text-right font-st text-xl font-light text-gray dark:text-beige lg:text-2xl xl:text-caption">
         {greetingWord}
       </div>
-      <div className="mt-12 h-full w-full xl:mt-[70px] xl:px-8 xl:pt-4">
+      <div className="mt-12 h-fit w-full xl:mt-[70px] xl:px-8 xl:pt-4">
         <div className="font-jk text-xl font-semibold text-orange selection:!bg-gray dark:text-green  dark:selection:!bg-beige sm:text-2xl lg:text-3xl xl:text-head2">
           {position + ' '}
           <br className="hidden xl:block" />

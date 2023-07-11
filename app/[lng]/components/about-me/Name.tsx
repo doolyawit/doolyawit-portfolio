aaff36f38fc1aa8866f9d53de3eff0d7278519a2
Doolyawit Yodkaew <dew.doolyawit@gmail.com>
2023-07-11 20:47:27 +0700

    style: adjust responsive design of introduction and about me section

--- app/[lng]/components/about-me/Name.tsx
+++ app/[lng]/components/about-me/Name.tsx
@@ -5,10 +5,10 @@ export default async function Name({ lng }: BaseProp) {
   const { t } = await useTranslation(lng);
 
   return (
-    <div className="relative h-1/3 text-right font-st text-base font-normal leading-snug xl:text-title">
+    <div className="relative h-1/3 text-right font-st text-sm font-normal leading-snug sm:text-lg md:text-2xl lg:text-title lg:leading-normal">
       {t('about_me.greeting')}
       <br />
-      <span className="underline-offset-[10px] text-decoration text-decoration-underline text-decoration-wavy text-decoration-green text-decoration-4 dark:text-decoration-orange">
+      <span className="underline-offset-2 text-decoration text-decoration-underline text-decoration-wavy text-decoration-green text-decoration-2 dark:text-decoration-orange md:underline-offset-4 lg:underline-offset-[10px] lg:text-decoration-4">
         {t('about_me.position')}
       </span>{' '}
       {t('developer')}

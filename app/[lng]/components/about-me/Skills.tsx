a7a592ad787529f7cb52fd8b2018d67e8b029feb
Doolyawit Yodkaew <dew.doolyawit@gmail.com>
2023-07-11 20:47:27 +0700

    style: adjust responsive design of introduction and about me section

--- app/[lng]/components/about-me/Skills.tsx
+++ app/[lng]/components/about-me/Skills.tsx
@@ -7,7 +7,7 @@ export default async function Skills({ lng }: BaseProp) {
   const { t } = await useTranslation(lng);
 
   return (
-    <div className="mb-8 mt-4">
+    <div className="mb-8 mt-8  md:mt-12 lg:mt-20 xl:mt-4">
       <SkillsLabel label={t('about_me.skill')} />
       <SkillsIcon />
     </div>
--- app/[lng]/components/about-me/SkillsLabel.tsx
+++ app/[lng]/components/about-me/SkillsLabel.tsx
@@ -3,9 +3,9 @@ import Underline from './svg/underline.svg';
 function SkillsLabel({ label }: { label: string }) {
   return (
     <div>
-      <div className="mb-2 text-center font-st text-3xl font-normal">{label}</div>
+      <div className="mb-2 text-center font-st text-xl font-normal md:text-2xl lg:text-3xl">{label}</div>
       <div className="flex content-center justify-center text-orange dark:text-green">
-        <Underline />
+        <Underline className={'w-[70px] lg:w-[105px]'} />
       </div>
     </div>
   );

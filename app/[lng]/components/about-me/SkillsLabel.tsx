5e7292b328e534be10b1af9dace4cc1852fcc606
Doolyawit Yodkaew <dew.doolyawit@gmail.com>
2023-07-12 11:55:00 +0700

    style: adjust responsive tablet size of about me section

--- app/[lng]/components/about-me/SkillsLabel.tsx
+++ app/[lng]/components/about-me/SkillsLabel.tsx
@@ -3,7 +3,7 @@ import Underline from './svg/underline.svg';
 function SkillsLabel({ label }: { label: string }) {
   return (
     <div>
-      <div className="mb-2 text-center font-st text-xl font-normal md:text-2xl lg:text-3xl">{label}</div>
+      <div className="mb-2 text-center font-st text-xl font-normal md:text-2xl lg:text-4xl xl:text-3xl">{label}</div>
       <div className="flex content-center justify-center text-orange dark:text-green">
         <Underline className={'w-[70px] lg:w-[105px]'} />
       </div>

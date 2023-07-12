df621db75f5b88ebca2416a1a992e6c396b06cfd
Doolyawit Yodkaew <dew.doolyawit@gmail.com>
2023-07-12 11:45:26 +0700

    style: adjust responsive of about me section

--- app/[lng]/components/about-me/AboutMe.tsx
+++ app/[lng]/components/about-me/AboutMe.tsx
@@ -6,12 +6,12 @@ import { BaseProp } from '../../../types';
 
 function AboutMe({ lng }: BaseProp) {
   return (
-    <div className="flex h-full w-full flex-col gap-8 lg:gap-10">
-      <div className="flex w-full gap-4 xl:h-[456px]">
-        <div className="h-full w-1/2 text-orange dark:text-green">
+    <div className="flex h-full w-full flex-col gap-2 lg:gap-10">
+      <div className="flex w-full flex-col gap-4 md:flex-row xl:h-[456px]">
+        <div className="h-full w-full text-orange dark:text-green md:w-1/2">
           <ProfilePhoto className={'flex items-start justify-start xl:h-[456px]'} />
         </div>
-        <div className="flex h-full w-1/2 flex-col justify-between gap-5 lg:gap-0">
+        <div className="flex h-full w-full flex-col justify-between gap-5 md:w-1/2 lg:gap-0">
           <Name lng={lng} />
           <Description lng={lng} />
         </div>
--- app/[lng]/components/about-me/Name.tsx
+++ app/[lng]/components/about-me/Name.tsx
@@ -5,7 +5,7 @@ export default async function Name({ lng }: BaseProp) {
   const { t } = await useTranslation(lng);
 
   return (
-    <div className="relative h-1/3 text-right font-st text-sm font-normal leading-snug sm:text-lg md:text-2xl lg:text-title lg:leading-normal">
+    <div className="relative h-1/3 text-center font-st text-2xl font-normal leading-normal md:text-right lg:text-title lg:leading-relaxed">
       {t('about_me.greeting')}
       <br />
       <span className="underline-offset-2 text-decoration text-decoration-underline text-decoration-wavy text-decoration-green text-decoration-2 dark:text-decoration-orange md:underline-offset-4 lg:underline-offset-[10px] lg:text-decoration-4">

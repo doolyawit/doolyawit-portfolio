0fc382f3cc63451914e4ce41d68bb87578713806
Doolyawit Yodkaew <dew.doolyawit@gmail.com>
2023-07-12 11:55:00 +0700

    style: adjust responsive tablet size of about me section

--- app/[lng]/components/introduction/NameAndPosition.tsx
+++ app/[lng]/components/introduction/NameAndPosition.tsx
@@ -12,7 +12,7 @@ export default async function NameAndPosition({ lng }: BaseProp) {
       <div className="ml-56 mt-2 flex w-full text-orange dark:text-green lg:ml-80 xl:-mt-6 xl:ml-[360px]">
         <StarSvg className="h-10 w-10 lg:h-12 lg:w-12 xl:h-14 xl:w-14" />
       </div>
-      <div className="flex w-full justify-center pt-4 text-center font-jk  text-5xl font-extrabold leading-normal text-gray selection:bg-green dark:text-beige dark:selection:bg-orange sm:px-20 sm:text-6xl sm:leading-relaxed lg:text-7xl lg:leading-relaxed xl:px-0 xl:pt-16 xl:text-head1">
+      <div className="flex w-full justify-center pt-4 text-center font-jk  text-5xl font-extrabold leading-normal text-gray selection:bg-green dark:text-beige dark:selection:bg-orange sm:px-20 sm:text-6xl sm:leading-relaxed lg:text-8xl lg:leading-relaxed xl:px-0 xl:pt-16 xl:text-head1">
         {t('doolyawit_yodkaew')}
       </div>
     </div>
